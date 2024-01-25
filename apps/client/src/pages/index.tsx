16929bea272e2a59eaae4f32d100736342a9238f
Md Aafaque Tabish <aafaque.tabish@gmail.com>
2024-01-25 21:24:08 +0530

    add ui as internal package for client

--- apps/client/src/pages/index.tsx
+++ apps/client/src/pages/index.tsx
@@ -2,13 +2,15 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import { Button } from "@repo/ui/button"
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
     <>
-        Hello World!
+      Hello World!
+      <Button appName="web" className={styles.button}>Click Here</Button>
     </>
   );
 }
