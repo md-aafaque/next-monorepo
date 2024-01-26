5b9087f3aa921fc9eae1c2341dd812ccca24e309
Md Aafaque Tabish <aafaque.tabish@gmail.com>
2024-01-26 11:23:31 +0530

    use the signup component in the signup page

--- apps/client/src/pages/signup.tsx
+++ apps/client/src/pages/signup.tsx
@@ -2,6 +2,20 @@ import { Signup } from '@repo/ui/components';
 
 export default function signupPage() {
     return <div>
-        <Signup></Signup>
+        <Signup onClick={async (email, password) => {
+            alert(email)
+            alert(password)
+        }}></Signup>
     </div>
 }
+{/* onClick={async() => {
+                        const response = await axios.post(`${BASE_URL}/admin/signup`, {
+                            username: email,
+                            password: password
+                        })
+                        let data = response.data;
+                        localStorage.setItem("token", data.token);
+                        // window.location = "/"
+                        setUser({userEmail: email, isLoading: false})
+                        router.push("/courses");
+                    }} */}
--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -1,2 +1,2 @@
 
-export * from "./signup";
+export * from "./Signup";
