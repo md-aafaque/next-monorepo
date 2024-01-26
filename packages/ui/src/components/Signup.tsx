f6c5735023770ae76232f0635c806fa8a42ba09c
Md Aafaque Tabish <aafaque.tabish@gmail.com>
2024-01-26 11:23:10 +0530

    add styles to Signup component

--- packages/ui/src/components/Signup.tsx
+++ packages/ui/src/components/Signup.tsx
@@ -1,8 +1,51 @@
-export function Signup() {
+import { Button, Card, TextField, Typography } from "@mui/material";
+import { useState } from "react";
+
+export function Signup(props: {onClick: (email: string, password: string) => void}) {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
 
     return <div>
-        <input type="text" placeholder="Email" />
-        <input type="text" placeholder="Password" />
-        <button>Submit</button>
+            <div style={{
+                paddingTop: 150,
+                marginBottom: 10,
+                display: "flex",
+                justifyContent: "center"
+            }}>
+                <Typography variant={"h6"}>
+                Welcome to Courzero. Sign up below
+                </Typography>
+            </div>
+        <div style={{display: "flex", justifyContent: "center"}}>
+            <Card variant={"outlined"} style={{width: 400, padding: 20}}>
+                <TextField
+                    onChange={(event) => {
+                        setEmail(event.target.value);
+                    }}
+                    fullWidth={true}
+                    label="Email"
+                    variant="outlined"
+                />
+                <br/><br/>
+                <TextField
+                    onChange={(e) => {
+                        setPassword(e.target.value);
+                    }}
+                    fullWidth={true}
+                    label="Password"
+                    variant="outlined"
+                    type={"password"}
+                />
+                <br/><br/>
+
+                <Button
+                    size={"large"}
+                    variant="contained"
+                    onClick={async() => {
+                        props.onClick(email, password);
+                    }}
+                > Signup</Button>
+            </Card>
+        </div>
     </div>
 }
