1fd116fa4598f1522921e0dc118b8df5aad3dbf1
Md Aafaque Tabish <aafaque.tabish@gmail.com>
2024-01-27 09:32:49 +0530

    connect signup component to backend and db

--- apps/client/src/pages/api/signup.ts
+++ apps/client/src/pages/api/signup.ts
@@ -2,6 +2,7 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import mongoose from 'mongoose';
 import { Admin } from "db";
 import jwt from "jsonwebtoken";
+import { ensureConnected } from "db";
 
 const SECRET = " SECRET";
 
@@ -19,8 +20,9 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<responseData>,
 ) {
+  await ensureConnected();
   const { email, password }: requestData = req.body;
-    const admin = await Admin.findOne({ email });
+  const admin = await Admin.findOne({ email });
   if (admin) {
     res.status(403).json({ message: 'Admin already exists' });
   } else {
