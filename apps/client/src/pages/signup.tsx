0e142382ab7a1949d40850ceab03ee65cbf93786
Md Aafaque Tabish <aafaque.tabish@gmail.com>
2024-01-27 09:33:36 +0530

    connect signup component to backend and db

--- apps/client/src/pages/signup.tsx
+++ apps/client/src/pages/signup.tsx
@@ -1,21 +1,15 @@
 import { Signup } from '@repo/ui/components';
+import axios from 'axios';
 
 export default function signupPage() {
     return <div>
         <Signup onClick={async (email, password) => {
-            alert(email)
-            alert(password)
+            const response = await axios.post("/api/signup", {
+                email: email,
+                password: password
+            })
+            let data = response.data;
+            localStorage.setItem("token", data.token);
         }}></Signup>
     </div>
 }
-{/* onClick={async() => {
-                        const response = await axios.post(`${BASE_URL}/admin/signup`, {
-                            username: email,
-                            password: password
-                        })
-                        let data = response.data;
-                        localStorage.setItem("token", data.token);
-                        // window.location = "/"
-                        setUser({userEmail: email, isLoading: false})
-                        router.push("/courses");
-                    }} */}
--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,3 +1,5 @@
 export * from "./models/admin";
 export * from "./models/user";
 export * from "./models/course";
+
+export * from "./dbConnect";
