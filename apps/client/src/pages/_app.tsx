58103e3852d5aa3af926abcce852b66f3a4956c9
Md Aafaque Tabish <aafaque.tabish@gmail.com>
2024-01-27 23:49:09 +0530

    add component to initialise user

--- apps/client/src/pages/_app.tsx
+++ apps/client/src/pages/_app.tsx
@@ -1,6 +1,52 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
+import { RecoilRoot, useSetRecoilState } from "recoil";
+import axios from "axios";
+import { useEffect } from "react";
+import { userState } from "store";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return <div>
+    <RecoilRoot>
+      <InitUser />
+      <Component {...pageProps} />;
+    </RecoilRoot>
+  </div>
+}
+
+const InitUser = () => {
+    const setUser = useSetRecoilState(userState);
+    const init = async() => {
+        try {
+            const response = await axios.get("api/admin/me", {
+                headers: {
+                    "Authorization": "Bearer " + localStorage.getItem("token")
+                }
+            })
+
+            if (response.data.username) {
+                setUser({
+                    isLoading: false,
+                    userEmail: response.data.username
+                })
+            } else {
+                setUser({
+                    isLoading: false,
+                    userEmail: null
+                })
+            }
+        } catch (e) {
+
+            setUser({
+                isLoading: false,
+                userEmail: null
+            })
+        }
+    };
+
+    useEffect(() => {
+        init();
+    }, []);
+
+    return <>Manoj</>
 }
